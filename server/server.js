74c7b78cc2f5e68ec89bf5b8478dab67e634e3bd
Alex Zai <azai91@gmail.com>
2015-04-04 13:40:29 -0700

    (fix) update server and index.html

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__path_+'/client/')); //serves the index.html
+app.use(express.static(__dirname)); //serves the index.html
 app.listen(3000); //listens on port 3000 -> http://localhost:3000/
