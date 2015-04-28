10da75ce9cf0e9cda0a0974f9af1f200b575c96f
formula1 <samtobia@gmail.com>
2015-04-27 20:21:05 -0700

    test able to be run by phantom js, included bad test

--- test/calendarTest.js
+++ test/calendarTest.js
@@ -1,35 +1,22 @@
-casper.test.begin("Calendar Spec", 12, function(test) {
-  casper.start('index.html', function() {
-    test.assertExists('.day','.day element exists');
-    test.assertElementCount('div.day', 2, '2 .day elements are present');
-  });
-
-  casper.then(function(){
-    test.assertExists('.week_number','.week_number element exists');
-    test.assertElementCount('p.week_number', 2, '2 .week_number elements are present');
-  });
-
-  casper.then(function(){
-    test.assertExists('.weekday','.weekday element exists');
-    test.assertElementCount('p.weekday', 2, '2 .weekday elements are present');
-  });
-
-  casper.then(function(){
-    test.assertExists('.challege','.challege exists');
-    test.assertElementCount('p.challege', 2, '2 .challege elements are present');
-  });
-
-  casper.then(function(){
-    test.assertExists('.goals','.goals exists');
-    test.assertElementCount('ul.goals', 2, '2 .goals ul elements are present');
-  });
-
-  casper.then(function(){
-    test.assertExists('.goal','.goal exists');
-    test.assertElementCount('li.goal', 6, '6 .goal li elements are present');
-  });
-
-  casper.run(function() {
-    test.done();
-  });
+describe('Calendar Spec', function() {
+  it('should have .day elements', function() {
+    expect(document.querySelector("div.day")).to.not.equal(null);
+    expect(document.querySelectorAll("div.day").length).to.equal(2);
+	});
+	it('should have .week_number elements',function(){
+    expect(document.querySelector(".week_number")).to.not.equal(null);
+    expect(document.querySelectorAll("p.week_number").length).to.equal(2);
+	});
+	it('should have .challenge elements',function(){
+    expect(document.querySelector(".challenge")).to.not.equal(null);
+    expect(document.querySelectorAll("p.challenge").length).to.equal(2);
+	});
+	it('should have .goals elements',function(){
+    expect(document.querySelector(".goals")).to.not.equal(null);
+    expect(document.querySelectorAll("ul.goals").length).to.equal(2);
+	});
+	it('should have .goal elements',function(){
+    expect(document.querySelector(".goal")).to.not.equal(null);
+    expect(document.querySelectorAll("li.goal").length).to.equal(6);
+	});
 });
