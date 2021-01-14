31dc3307fb600fd5aec2973e839e2c7bbff0b04c
Will Kencel <wkencel1@gmail.com>
2021-01-13 21:32:01 -0500

    stopped console from showing up on load
    electron work 9:31 PM 1/13/21

--- electron/main.ts
+++ electron/main.ts
@@ -8,9 +8,18 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    // so the app will show
+    // show: true,
+    // frame: false,
+    // fullscramble: false,
+    // // can resize frame
+    // resizeable: true,
+    // transparent: false,
+    //   // stops console from opening upon load
     webPreferences: {
       nodeIntegration: true,
       webSecurity: false,
+      devTools: false,
     },
   });
 
@@ -27,10 +36,20 @@ function createWindow() {
     );
   }
 
-  mainWindow.on("closed", () => {
-    mainWindow = null;
-  });
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
 }
 
 app.on("ready", createWindow);
+
+app.on('activate', function () {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
+
 app.allowRendererProcessReuse = true;
