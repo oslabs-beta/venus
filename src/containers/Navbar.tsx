1ee083c566bc013724e11326a3d3a7dd91967aeb
evperlman <evperlman@gmail.com>
2021-01-16 14:43:26 -0800

    working context

--- src/containers/Navbar.tsx
+++ src/containers/Navbar.tsx
@@ -3,13 +3,33 @@
  * @desc Left-hand side of Main Display. Displays Navigation Bar
  */
 
-import React, { Component } from 'react';
+import React, { Component, useContext, useEffect, useState } from 'react';
 import TabContainer from './TabContainer'
+import { myContext } from '../contexts/globalContext';
 
 export default function  NavBar(): JSX.Element {
+	const [will, setWill] = useState('')
+	
+	const { urls, setUrls } = useContext(myContext);
+	let test = urls
+	useEffect(() => {
+		setUrls(['mike', 'evan'])
+		setWill('will')
+	}, []);
+
+	const handlePress = () => {
+		setUrls(['new', 'state'])
+
+	}
+
+
+	console.log(urls, "navbar")
   return(
 		<div id="navBar">
 			<TabContainer />
+			<input type="button" onClick={handlePress}></input>
+			<h1>{test}</h1>
+			<h1>{will}</h1>
     </div>
   ) 
  }
---
+++ src/contexts/dynamicContext.tsx
@@ -0,0 +1,28 @@
+import React, { useState} from 'react'; 
+
+type dynamicState = {
+  services: {
+    host: string,
+    port: string,
+    path: string, 
+    date: string, // potentially an int TBD.
+    statusCode: number, 
+    latency: number, 
+  }[];
+  setServices: (input:any[]) => void;
+};
+export const liveData: dynamicState= {
+  services: [],
+  setServices: () => {}
+};
+
+export const dynamicContext = React.createContext<dynamicState>(liveData)
+
+
+export const ContextProvider: React.FC = (props: any) => {
+  
+  const [services, setServices] = useState<any[]>([]);
+
+  return <dynamicContext.Provider value={{services, setServices}}>{props.children}</dynamicContext.Provider>
+
+}
