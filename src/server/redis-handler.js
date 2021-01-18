d38494c08258b7419a00cc1ad5dd34c6485c9d3d
Akshay Suggula <akshaysuggula@akshays-mbp.lan>
2021-01-18 15:45:57 -0800

    added all functionality for redis handler

--- src/server/redis-handler.js
+++ src/server/redis-handler.js
@@ -4,4 +4,51 @@ const cluster = new Redis.Cluster([{
   host: 'venus-redis-cluster-2.syohjt.clustercfg.use2.cache.amazonaws.com'
 }]);
 
+const STREAM_KEY = 'logstream'
 
+console.log('something is happening'); 
+
+const readRedisStream = async () => {
+
+  let streamEntries = await cluster.xrange(STREAM_KEY, '-', '+', 'COUNT', 1); 
+
+  console.log('XRANGE, standard response:'); 
+  console.log(streamEntries); 
+
+  Redis.Command.setReplyTransformer('xrange', function (result) {
+    if(Array.isArray(result)){
+      const newResult = []; 
+      for(const r of result){
+        const obj = {
+          id: r[0]
+        }; 
+
+        const fieldNamesValues = r[1]; 
+
+        for(let i = 0; i < fieldNamesValues.length; i += 2){
+          const k = fieldNamesValues[n]; 
+          const v = fieldNamesValues[n + 1]; 
+          obj[k] = v; 
+        }
+
+        newResult.push(obj); 
+      }
+
+      return newResult; 
+    }
+
+    return result; 
+  }); 
+
+  streamEntries = await cluster.xrange(STREAM_KEY, '-', '+', 'COUNT', 1); 
+
+  console.log('XRANGE, response with reply transformer'); 
+  console.log(streamEntries); 
+
+}
+
+try {
+  readRedisStream(); 
+} catch (e) {
+  console.error(e); 
+}
