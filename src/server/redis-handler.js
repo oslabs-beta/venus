1e041b1e50d1fd0b1f970d10bac55893b6c62463
Akshay Suggula <akshaysuggula@akshays-mbp.lan>
2021-01-18 15:53:14 -0800

    trying to read two entries

--- src/server/redis-handler.js
+++ src/server/redis-handler.js
@@ -10,7 +10,7 @@ console.log(`Reading the stream named ${STREAM_KEY}...`);
 
 const readRedisStream = async () => {
 
-  let streamEntries = await cluster.xrange(STREAM_KEY, '-', '+', 'COUNT', 1); 
+  let streamEntries = await cluster.xrange(STREAM_KEY, '-', '+', 'COUNT', 2); 
 
   console.log('XRANGE, standard response:'); 
   console.log(streamEntries); 
@@ -40,7 +40,7 @@ const readRedisStream = async () => {
     return result; 
   }); 
 
-  streamEntries = await cluster.xrange(STREAM_KEY, '-', '+', 'COUNT', 1); 
+  streamEntries = await cluster.xrange(STREAM_KEY, '-', '+', 'COUNT', 2); 
 
   console.log('XRANGE, response with reply transformer'); 
   console.log(streamEntries); 
