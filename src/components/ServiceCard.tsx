3f879c561d069fe90ce813f24de00c95e52704d3
evperlman <evperlman@gmail.com>
2021-01-19 00:04:04 -0800

    updated service components

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -9,38 +9,40 @@ import Typography from 'antd/es/typography';
 // import { CheckCircleFilled } from '@ant-design/icons';
 const { Title } = Typography
 
-function ServiceCard (): JSX.Element{
+function ServiceCard (props: { service: any }): JSX.Element{
   const { urls } = useContext(myContext)
+  console.log(props)
+  const color: string = props.color
     return (
       (
         <div className= "aggregateStats">
   
-            <Title level={3}>Overall Health</Title>
+            <Title level={3}>{props.name}</Title>
             {/* <CheckCircleFilled /> */}
-            <div className="icon"></div>
+            <div className="icon" style={{backgroundColor: color}}></div>
           <div className="allStats">
             <div className="statsContainer">
               <Title level={5}>Uptime</Title>
               <div className="dataContainer">
-                <Title level={5}>97%</Title>
+                <Title level={5}>{props.uptime}</Title>
               </div>
             </div>
             <div className="statsContainer">
               <Title level={5}>Latency</Title>
               <div className="dataContainer">
-                <Title level={5}>250ms</Title>
+                <Title level={5}>{props.latency}</Title>
               </div>
             </div>
             <div className="statsContainer">
               <Title level={5}>Load</Title>
               <div className="dataContainer">
-                <Title level={5}>50req/min</Title>
+                <Title level={5}>{props.load}</Title>
               </div>
             </div>
             <div className="statsContainer">
               <Title level={5}>Error %</Title>
               <div className="dataContainer">
-                <Title level={5}>3.413%</Title>
+                <Title level={5}>{props.error}</Title>
               </div>
             </div>
           </div>
