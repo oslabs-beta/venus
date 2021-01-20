c96429d19944d950d916464e99c854069b0e1da9
Akshay Suggula <akshaysuggula@akshays-mbp.lan>
2021-01-19 23:29:25 -0800

    TEST DELETE

--- src/server/historical-read-handler.js
+++ src/server/historical-read-handler.js
@@ -79,8 +79,10 @@ const readAndWriteToDB = async () => {
 
   //QUERY STREAM
 
-  console.log('mostRecentTimeStamp BEING USED FOR REDIS QUERY: ', mostRecentTimeStamp); 
-  streamEntries = await redis.xread('STREAMS', STREAM_KEY, mostRecentTimeStamp); 
+  console.log('mostRecentTimeStamp BEING USED FOR REDIS QUERY: ', mostRecentTimeStamp);
+
+  // streamEntries = await redis.xread('STREAMS', STREAM_KEY, mostRecentTimeStamp); 
+  streamEntries = await redis.xread('BLOCK', PING_RATE, 'STREAMS', STREAM_KEY, '$'); 
 
   console.log('XREAD, response with reply transformer'); 
   // //real-time entries should be sent for processing elsewhere 
