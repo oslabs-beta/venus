3e117796c8ce430281470c9f68b2150a90363c5e
Akshay Suggula <akshaysuggula@akshays-mbp.lan>
2021-01-20 00:06:55 -0800

    REVERTING BACK TO XREAD FOR HISTORICAL ANALYZER

--- src/server/historical-read-handler.js
+++ src/server/historical-read-handler.js
@@ -53,79 +53,79 @@ const readAndWriteToDB = async () => {
   // })
 
   //Get the milliseconds for start and end time
-  const startTime = Date.now() - INTERVAL; 
-  const endTime = startTime + INTERVAL;  
+  // const startTime = Date.now() - INTERVAL; 
+  // const endTime = startTime + INTERVAL;  
 
   // // //Transform xread's output from two arrays of keys and value into one array of log objects
-  // Redis.Command.setReplyTransformer('xread', function (result) {
-  //   if(Array.isArray(result)){
-  //     const newResult = []; 
-  //     for(const log of result[0][1]){
-  //       const obj = {
-  //         id: log[0]
-  //       }; 
+  Redis.Command.setReplyTransformer('xread', function (result) {
+    if(Array.isArray(result)){
+      const newResult = []; 
+      for(const log of result[0][1]){
+        const obj = {
+          id: log[0]
+        }; 
 
-  //       const fieldNamesValues = log[1]; 
+        const fieldNamesValues = log[1]; 
 
-  //       for(let i = 0; i < fieldNamesValues.length; i+=2){
-  //           const k = fieldNamesValues[i]; 
-  //           const v = fieldNamesValues[i + 1]; 
-  //           obj[k] = v; 
-  //       }
-  //       newResult.push(obj); 
-  //     }
-
-  //     return newResult; 
-  //   }
-
-  //   return result; 
-  // }); 
-
-    //Transform xrange's output from two arrays of keys and value into one array of log objects
-    Redis.Command.setReplyTransformer('xrange', function (result) {
-      if(Array.isArray(result)){
-        const newResult = []; 
-        for(const r of result){
-          const obj = {
-            id: r[0]
-          }; 
-  
-          const fieldNamesValues = r[1]; 
-  
-          for(let i = 0; i < fieldNamesValues.length; i += 2){
+        for(let i = 0; i < fieldNamesValues.length; i+=2){
             const k = fieldNamesValues[i]; 
             const v = fieldNamesValues[i + 1]; 
             obj[k] = v; 
-          }
-  
-          newResult.push(obj); 
         }
-  
-        return newResult; 
+        newResult.push(obj); 
       }
+
+      return newResult; 
+    }
+
+    return result; 
+  }); 
+
+    //Transform xrange's output from two arrays of keys and value into one array of log objects
+    // Redis.Command.setReplyTransformer('xrange', function (result) {
+    //   if(Array.isArray(result)){
+    //     const newResult = []; 
+    //     for(const r of result){
+    //       const obj = {
+    //         id: r[0]
+    //       }; 
   
-      return result; 
-    }); 
+    //       const fieldNamesValues = r[1]; 
+  
+    //       for(let i = 0; i < fieldNamesValues.length; i += 2){
+    //         const k = fieldNamesValues[i]; 
+    //         const v = fieldNamesValues[i + 1]; 
+    //         obj[k] = v; 
+    //       }
+  
+    //       newResult.push(obj); 
+    //     }
+  
+    //     return newResult; 
+    //   }
+  
+    //   return result; 
+    // }); 
 
   //QUERY STREAM
 
   // streamEntries = await redis.xread('STREAMS', STREAM_KEY, mostRecentTimeStamp); 
-  streamEntries = await redis.xrange(STREAM_KEY, startTime, endTime);
-  // streamEntries = await redis.xread('BLOCK', PING_RATE, 'STREAMS', STREAM_KEY, '$'); 
+  // streamEntries = await redis.xrange(STREAM_KEY, startTime, endTime);
+  streamEntries = await redis.xread('BLOCK', PING_RATE, 'STREAMS', STREAM_KEY, '$'); 
 
-  console.log('XRANGE, response with reply transformer'); 
+  console.log('XREAD, response with reply transformer'); 
   // //real-time entries should be sent for processing elsewhere 
   console.log(streamEntries); 
 
+  console.log(`Writing to table ${DB_NAME}...`); 
+
   //WRITE TO THE DATABASE
+
   let queryText = `INSERT INTO ${TABLE_NAME} (redis_timestamp, req_method, req_host, req_path, req_url, res_status_code, res_message, cycle_duration) VALUES `; 
 
-  if(streamEntries.length !== 0){
-
-    console.log(`Writing to table ${DB_NAME}...`);
-
+  if(streamEntries){
     streamEntries.forEach( (log) => {
-      console.log('log: ', log); 
+      // console.log('log: ', log); 
       queryText += `('${log.id}', '${log.reqMethod}', '${log.reqHost}', '${log.reqPath}', '${log.reqURL}', '${log.resStatusCode}', '${log.resMessage}', '${log.cycleDuration}'),`; 
     })
   
@@ -147,7 +147,20 @@ const readAndWriteToDB = async () => {
 }
 
 try {
-  setInterval(async () => { await readAndWriteToDB() }, PING_RATE); 
+  setInterval(async () => { 
+    // await client.query('SELECT * FROM logs LIMIT 1;', (err, result) => {
+    //   if(err){
+    //     console.log(err); 
+    //   } else {
+    //     // console.log('result from limit 1 query: ',result); 
+    //     mostRecentTimeStamp = result.rows[0].redis_timestamp; 
+    //     // console.log('mostRecentTimeStamp: ', mostRecentTimeStamp); 
+    //     readAndWriteToDB(); 
+    //   }
+    // })
+
+    readAndWriteToDB();
+  }, PING_RATE); 
 } catch (e) {
   console.error(e); 
 }
