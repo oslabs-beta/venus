4682c30476fb35b1bf5f88ebcde258b62406b1dc
Akshay Suggula <akshaysuggula@akshays-mbp.lan>
2021-01-19 21:32:38 -0800

    revised sql script

--- src/server/postgres-create-script.sql
+++ src/server/postgres-create-script.sql
@@ -1,9 +1,11 @@
+DROP TABLE logs CASCADE; 
+
 CREATE TABLE logs (
-    "id" serial NOT NULL,
+    "id" varchar NOT NULL,
     "req_method" varchar NOT NULL,
     "req_host" varchar NOT NULL,
     "req_path" varchar NOT NULL,
-    "req_URL" varchar NOT NULL,
+    "req_url" varchar NOT NULL,
     "res_status_code" varchar NOT NULL,
     "res_message" varchar NOT NULL,
     "cycle_duration" varchar NOT NULL,
