b645db7cff8ed506e5addb50530330dddddfd5a5
Akshay Suggula <akshaysuggula@akshays-mbp.lan>
2021-01-20 19:05:47 -0800

    added port to env file

--- src/server/historical-read-handler.js
+++ src/server/historical-read-handler.js
@@ -33,7 +33,7 @@ const client = new Client({
   host: process.env.DB_HOST, 
   database: process.env.DB_NAME, 
   password: process.env.DB_PASS, 
-  port: 5432
+  port: process.env.DB_PORT
 })
 
 client.connect(); 
