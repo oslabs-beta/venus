c190d17de491f262963a0939cecec2d0645ee8c1
Will Kencel <wkencel1@gmail.com>
2021-01-22 00:33:08 -0500

    created brush chart
    1 of two charts for area graph ( rendered as brush chart in charts  container)

---
+++ src/charts/CyclePerformanceChart.tsx
@@ -0,0 +1,252 @@
+import React, { useRef, useState, useMemo } from "react";
+import { scaleTime, scaleLinear } from "@visx/scale";
+// redeclare data
+// import appleStock, { AppleStock } from "@visx/mock-data/lib/mocks/appleStock";
+import { histData, historicalContext} from '../contexts/historicalContext' ; 
+import { Brush } from "@visx/brush";
+import { Bounds } from "@visx/brush/lib/types";
+import BaseBrush, {
+  BaseBrushState,
+  UpdateBrush
+} from "@visx/brush/lib/BaseBrush";
+import { PatternLines } from "@visx/pattern";
+import { LinearGradient } from "@visx/gradient";
+import { max, extent } from "d3-array";
+// import
+import AreaChartCycle from "./AreaChartCycle";
+
+// model of mockdata from applestock
+export interface AppleStock {
+  date: string;
+  close: number;
+}
+
+export const appleStock: AppleStock[] = [
+  { date: '2007-04-24T07:00:00.000Z', close: 93.24 },
+  { date: '2007-05-24T07:00:00.000Z', close: 93.24 },
+  { date: '2007-06-24T07:00:00.000Z', close: 93.24 },
+  { date: '2007-07-24T07:00:00.000Z', close: 93.24 }
+];
+
+// date is same i.e. time request was sent
+// becomes x axis
+// close becomes cycle duration i.e. 1136 ms and becomes y axis
+
+// Initialize some variables
+const stock = appleStock;
+const brushMargin = { top: 20, bottom: 5, left: 50, right: 20 };
+const chartSeparation = 30;
+const PATTERN_ID = "brush_pattern";
+const GRADIENT_ID = "brush_gradient";
+export const background = 'white';
+export const background2 = 'rgb(93, 2, 116)';
+export const background3 = 'rgb(230, 92, 0)';
+export const accentColor = '#edffea';
+export const accentColorDark = '#75daad';
+const selectedBrushStyle = {
+  fill: `url(#${PATTERN_ID})`,
+  stroke: "white"
+};
+
+// accessors
+const getDate = (d: AppleStock) => new Date(d.date);
+const getStockValue = (d: AppleStock) => d.close;
+
+export type BrushProps = {
+  width: number;
+  height: number;
+  margin?: { top: number; right: number; bottom: number; left: number };
+  compact?: boolean;
+};
+
+function BrushChart({
+  compact = false,
+  width,
+  height,
+  margin = {
+    top: 20,
+    left: 50,
+    bottom: 25,
+    right: 20
+  }
+}: BrushProps) {
+  const brushRef = useRef<BaseBrush | null>(null);
+  const [filteredStock, setFilteredStock] = useState(stock);
+
+  const onBrushChange = (domain: Bounds | null) => {
+    if (!domain) return;
+    const { x0, x1, y0, y1 } = domain;
+    const stockCopy = stock.filter((s) => {
+      const x = getDate(s).getTime();
+      const y = getStockValue(s);
+      return x > x0 && x < x1 && y > y0 && y < y1;
+    });
+    setFilteredStock(stockCopy);
+  };
+
+  const innerHeight = height - margin.top - margin.bottom;
+  const topChartBottomMargin = compact
+    ? chartSeparation / 2
+    : chartSeparation + 12;
+  const topChartHeight = 0.8 * innerHeight - topChartBottomMargin;
+  const bottomChartHeight = innerHeight - topChartHeight - chartSeparation;
+
+  // bounds
+  const xMax = Math.max(width - margin.left - margin.right, 0);
+  const yMax = Math.max(topChartHeight, 0);
+  const xBrushMax = Math.max(width - brushMargin.left - brushMargin.right, 0);
+  const yBrushMax = Math.max(
+    bottomChartHeight - brushMargin.top - brushMargin.bottom,
+    0
+  );
+
+  // scales
+  const dateScale = useMemo(
+    () =>
+      scaleTime<number>({
+        range: [0, xMax],
+        domain: extent(filteredStock, getDate) as [Date, Date]
+      }),
+    [xMax, filteredStock]
+  );
+  const stockScale = useMemo(
+    () =>
+      scaleLinear<number>({
+        // here you can use range to establish the shown height of y like how much of the screen it takes up 
+       
+        range: [yMax, 110],
+        // here you can use domain to establish the bottom of the y axis
+        domain: [200, max(filteredStock, getStockValue) || 0],
+        nice: true
+      }),
+    [yMax, filteredStock]
+  );
+  const brushDateScale = useMemo(
+    () =>
+      scaleTime<number>({
+        range: [0, xBrushMax],
+        domain: extent(stock, getDate) as [Date, Date]
+      }),
+    [xBrushMax]
+  );
+  const brushStockScale = useMemo(
+    () =>
+      scaleLinear({
+        range: [yBrushMax, 0],
+        domain: [200, max(stock, getStockValue) || 0],
+        nice: true
+      }),
+    [yBrushMax]
+  );
+
+  const initialBrushPosition = useMemo(
+    () => ({
+      start: { x: brushDateScale(getDate(stock[50])) },
+      end: { x: brushDateScale(getDate(stock[150])) }
+    }),
+    [brushDateScale]
+  );
+
+  // event handlers
+  const handleClearClick = () => {
+    if (brushRef?.current) {
+      setFilteredStock(stock);
+      brushRef.current.reset();
+    }
+  };
+
+  const handleResetClick = () => {
+    if (brushRef?.current) {
+      const updater: UpdateBrush = (prevBrush) => {
+        const newExtent = brushRef.current!.getExtent(
+          initialBrushPosition.start,
+          initialBrushPosition.end
+        );
+
+        const newState: BaseBrushState = {
+          ...prevBrush,
+          start: { y: newExtent.y0, x: newExtent.x0 },
+          end: { y: newExtent.y1, x: newExtent.x1 },
+          extent: newExtent
+        };
+
+        return newState;
+      };
+      brushRef.current.updateBrush(updater);
+    }
+  };
+
+  console.log('let lowercase, then uppercase ', appleStock, AppleStock, 'live data freq ', histData.historical)
+
+  return (
+    <div>
+      <svg width={width} height={height}>
+        <LinearGradient
+          id={GRADIENT_ID}
+          from={background}
+          to={background2}
+          to={background3}
+          rotate={30}
+        />
+        <rect
+          x={0}
+          y={0}
+          width={width}
+          height={height}
+          fill={`url(#${GRADIENT_ID})`}
+          rx={14}
+        />
+        <AreaChartCycle
+          hideBottomAxis={compact}
+          data={filteredStock}
+          width={width}
+          margin={{ ...margin, bottom: topChartBottomMargin }}
+          yMax={yMax}
+          xScale={dateScale}
+          yScale={stockScale}
+          gradientColor={background2}
+        />
+        <AreaChartCycle
+          hideBottomAxis
+          hideLeftAxis
+          data={stock}
+          width={width}
+          yMax={yBrushMax}
+          xScale={brushDateScale}
+          yScale={brushStockScale}
+          margin={brushMargin}
+          top={topChartHeight + topChartBottomMargin + margin.top}
+          gradientColor={background2}
+        >
+          <PatternLines
+            id={PATTERN_ID}
+            height={8}
+            width={8}
+            stroke={accentColor}
+            strokeWidth={1}
+            orientation={["diagonal"]}
+          />
+          <Brush
+            xScale={brushDateScale}
+            yScale={brushStockScale}
+            width={xBrushMax}
+            height={yBrushMax}
+            margin={brushMargin}
+            handleSize={8}
+            innerRef={brushRef}
+            resizeTriggerAreas={["left", "right"]}
+            brushDirection="horizontal"
+            initialBrushPosition={initialBrushPosition}
+            onChange={onBrushChange}
+            onClick={() => setFilteredStock(stock)}
+            selectedBoxStyle={selectedBrushStyle}
+          />
+        </AreaChartCycle>
+      </svg>
+      {/* <button onClick={handleClearClick}>Clear</button>&nbsp;
+      <button onClick={handleResetClick}>Reset</button> */}
+    </div>
+  );
+}
+
+export default BrushChart;
