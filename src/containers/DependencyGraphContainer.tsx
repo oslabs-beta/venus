c6fdf09ab515ef48974bc223700a7aa1be7c7ab7
Will Kencel <wkencel1@gmail.com>
2021-01-22 23:29:24 -0500

    finished dependency graph

--- src/containers/DependencyGraphContainer.tsx
+++ src/containers/DependencyGraphContainer.tsx
@@ -1,12 +1,17 @@
 import React, { Component, useContext, useEffect, useState } from 'react';
 import { DependencyTree } from '../charts/DependencyTree'
 import Card from 'antd/es/card';
+import { DependencyGraph} from '../charts/DependencyTree'
+import ParentSize from "@visx/responsive/lib/components/ParentSize";
+
 function DependencyGraphContainer(): JSX.Element{
   
   return(
     <div id="dashboard">
       <Card hoverable={true}>
-        <DependencyTree />
+        <ParentSize>
+          {({ width, height }) => <DependencyGraph width={950} height={900} />}
+        </ParentSize>
         dependencyGraph will go here.
       </Card>
     </div>
