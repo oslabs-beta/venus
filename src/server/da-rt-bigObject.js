8cba9f17b0d84c831a571a1f0974e7ed50a30ccd
Vlad <munteanuv@gmail.com>
2021-01-23 21:07:37 -0800

    full consolidated object with method-level data

--- src/server/da-rt-bigObject.js
+++ src/server/da-rt-bigObject.js
@@ -102,41 +102,56 @@ const responseData = [
 // console.log(uniqueHosts);
 
 const rtData = (data) => {
-  
-  const dataframes = []; // create an array of dataframes to work with 
-  
+  const dataframes = []; // create an array of dataframes to work with
+
   let df = new dfd.DataFrame(data);
 
-  const dfGroup = df.groupby(['reqHost']);
+  const dfGroup = df.groupby(["reqHost"]);
   // const dfNew = new dfd.DataFrame(dfGroup.data);
-  const dfNew = dfGroup.col(['reqHost']).count();
+  const dfNew = dfGroup.col(["reqHost"]).count();
 
-  const errorResRows = df.query({ column: 'resStatusCode', is: ">=", to: 400 });
-  
-  const errorDF = errorResRows.groupby(['reqHost']); 
-  const newErrorDF = errorDF.col(['resStatusCode']).count(); 
+  const errorResRows = df.query({ column: "resStatusCode", is: ">=", to: 400 });
+
+  const errorDF = errorResRows.groupby(["reqHost"]);
+  const newErrorDF = errorDF.col(["resStatusCode"]).count();
   // newErrorDF.print();
-  
+
   dfNew.print();
 
-  let finalTable = dfd.merge({ left: dfNew, right: newErrorDF, on:['reqHost'], how: 'left' });
+  let finalTable = dfd.merge({
+    left: dfNew,
+    right: newErrorDF,
+    on: ["reqHost"],
+    how: "left",
+  });
 
-  const latencyDF = dfGroup.col(['cycleDuration']).mean(); 
-  
+  const latencyDF = dfGroup.col(["cycleDuration"]).mean();
 
-  finalTable = dfd.merge({ left: finalTable, right: latencyDF, on:['reqHost'], how: 'left' });
+  finalTable = dfd.merge({
+    left: finalTable,
+    right: latencyDF,
+    on: ["reqHost"],
+    how: "left",
+  });
 
-  const availabilityDF = dfGroup.col(['resStatusCode']).count();
+  const availabilityDF = dfGroup.col(["resStatusCode"]).count();
   // availabilityDF.print()
-  finalTable = dfd.merge({ left: finalTable, right: availabilityDF, on:['reqHost'], how: 'left' });
+  finalTable = dfd.merge({
+    left: finalTable,
+    right: availabilityDF,
+    on: ["reqHost"],
+    how: "left",
+  });
 
-  finalTable.fillna({ values: [0], inplace: true }); 
+  finalTable.fillna({ values: [0], inplace: true });
 
-  //   const totalResponses = finalTable.loc({ columns: ['resStatusCode_1'] }); 
-  //   const errorResponses = finalTable.loc({ columns: ['resStatusCode'] }); 
-  
-  const errorRateCol = finalTable[`resStatusCode_count`].div(finalTable[`resStatusCode_count_1`]).mul(100);
-  
+  //   const totalResponses = finalTable.loc({ columns: ['resStatusCode_1'] });
+  //   const errorResponses = finalTable.loc({ columns: ['resStatusCode'] });
+
+  const errorRateCol = finalTable["resStatusCode_count"]
+    .div(finalTable["resStatusCode_count_1"])
+    .mul(100);
+
   //   const errorRate = errorRequests.div(totalRequests).mul(100);
   //   // console.log(errorRate);
   finalTable.addColumn({
@@ -144,81 +159,197 @@ const rtData = (data) => {
     value: errorRateCol.col_data[0],
   });
 
-    
-  // console.log(finalTable); 
-  
-  const availabilityCol = finalTable[`resStatusCode_count_1`].div(finalTable[`reqHost_count`]).mul(100);
-  
+  // console.log(finalTable);
+
+  const availabilityCol = finalTable["resStatusCode_count_1"]
+    .div(finalTable["reqHost_count"])
+    .mul(100);
+
   // availabilityCol.print();
   finalTable.addColumn({
-    column: 'Availability (%)',
+    column: "Availability (%)",
     value: availabilityCol.col_data[0],
   });
 
-  let outputTable = finalTable.loc({ columns: ['reqHost', 'reqHost_count', 'cycleDuration_mean', 'Error (%)', 'Availability (%)'] });
-
-  outputTable[`reqHost_count`] = outputTable[`reqHost_count`].div(3);
-
-  console.log('Final Table: ', outputTable); 
-  outputTable.print(); 
-
-  const consolidatedObj = {};
-  consolidatedObj.services = []; 
-  
-  
-  outputTable.data.forEach((row) => {
-    const outputObj = {}; 
-
-    outputObj.service = row[0];
-    outputObj.status = 'good'; 
-    outputObj.load = row[1] + ' hpm';
-    outputObj.response_time = row[2];
-    outputObj.error = row[3];
-    outputObj.availability = row[4]; 
-    
-    consolidatedObj.services.push(outputObj); 
+  let outputTable = finalTable.loc({
+    columns: [
+      "reqHost",
+      "reqHost_count",
+      "cycleDuration_mean",
+      "Error (%)",
+      "Availability (%)",
+    ],
   });
 
-  // console.log(consolidatedObj); 
+  outputTable["reqHost_count"] = outputTable["reqHost_count"].div(3);
 
-  // df.print();  
-  
+  console.log("Final Table: ", outputTable);
+  outputTable.print();
+
+  const consolidatedObj = {};
+  consolidatedObj.services = [];
+
+  outputTable.data.forEach((row) => {
+    const outputObj = {};
+
+    outputObj.service = row[0];
+    outputObj.status = "good";
+    outputObj.load = row[1].toFixed(2) + " hpm";
+    outputObj.response_time = row[2];
+    outputObj.error = row[3];
+    outputObj.availability = row[4];
+
+    consolidatedObj.services.push(outputObj);
+  });
+
+  // console.log(consolidatedObj);
+
+  // df.print();
 
   //FOR OVERALL AGGREGATE STATISTICS
 
-  consolidatedObj.aggregate = {}; 
+  consolidatedObj.aggregate = {};
 
-  const errorResRowsTotal = df.query({ column: 'resStatusCode', is: ">=", to: 400 }); 
-  
-  const newErrorDFTotal = errorResRowsTotal['resStatusCode'].count(); 
-  const totalRequests = df['reqHost'].count(); 
-  const totalResponses = df['resStatusCode'].count(); 
-  consolidatedObj.aggregate.status = 'good';
-  consolidatedObj.aggregate.load = (totalRequests / STREAM_WINDOW) + ' hpm';
-  consolidatedObj.aggregate.response_time = Math.round(df['cycleDuration'].mean());
+  const errorResRowsTotal = df.query({
+    column: "resStatusCode",
+    is: ">=",
+    to: 400,
+  });
+
+  const newErrorDFTotal = errorResRowsTotal["resStatusCode"].count();
+  const totalRequests = df["reqHost"].count();
+  const totalResponses = df["resStatusCode"].count();
+  consolidatedObj.aggregate.status = "good";
+  consolidatedObj.aggregate.load = totalRequests / STREAM_WINDOW + " hpm";
+  consolidatedObj.aggregate.response_time = Math.round(
+    df["cycleDuration"].mean()
+  );
   consolidatedObj.aggregate.error = (newErrorDFTotal / totalResponses) * 100;
-  consolidatedObj.aggregate.availability = Math.round((totalResponses / totalRequests) * 100);
-  
+  consolidatedObj.aggregate.availability = Math.round(
+    (totalResponses / totalRequests) * 100
+  );
 
-  console.log(consolidatedObj); 
+  console.log(consolidatedObj);
 
   const uniqueHosts = df["reqHost"].unique().data;
 
-  uniqueHosts.forEach(host => {
-    
-  })
+  uniqueHosts.forEach((host) => {
+    const dfHost = df.query({ column: "reqHost", is: "==", to: host });
+
+    const dfGroupMethod = dfHost.groupby(["reqMethod"]);
+    const dfNewMethod = dfGroupMethod.col(["reqMethod"]).count();
+
+    try {
+      const errorResRowsMethod = dfHost.query({
+        column: "resStatusCode",
+        is: ">=",
+        to: 400,
+      });
+
+      const errorDFMethod = errorResRowsMethod.groupby(["reqMethod"]);
+      const newErrorDFMethod = errorDFMethod.col(["resStatusCode"]).count();
+      // newErrorDFMethod.print();
+
+      // dfNew.print();
+
+      let finalTableMethod = dfd.merge({
+        left: dfNewMethod,
+        right: newErrorDFMethod,
+        on: ["reqMethod"],
+        how: "left",
+      });
+
+      const latencyDFMethod = dfGroupMethod.col(["cycleDuration"]).mean();
+
+      finalTableMethod = dfd.merge({
+        left: finalTableMethod,
+        right: latencyDFMethod,
+        on: ["reqMethod"],
+        how: "left",
+      });
+
+      const availabilityDFMethod = dfGroupMethod.col(["resStatusCode"]).count();
+      // availabilityDF.print()
+      finalTableMethod = dfd.merge({
+        left: finalTableMethod,
+        right: availabilityDFMethod,
+        on: ["reqMethod"],
+        how: "left",
+      });
+
+      finalTableMethod.fillna({ values: [0], inplace: true });
+
+      //   const totalResponses = finalTable.loc({ columns: ['resStatusCode_1'] });
+      //   const errorResponses = finalTable.loc({ columns: ['resStatusCode'] });
+
+      const errorRateColMethod = finalTableMethod["resStatusCode_count"]
+        .div(finalTableMethod["resStatusCode_count_1"])
+        .mul(100);
+
+      //   const errorRate = errorRequests.div(totalRequests).mul(100);
+      //   // console.log(errorRate);
+      finalTableMethod.addColumn({
+        column: "Error (%)",
+        value: errorRateColMethod.col_data[0],
+      });
+
+      // console.log(finalTableMethod);
+
+      const availabilityColMethod = finalTableMethod["resStatusCode_count_1"]
+        .div(finalTableMethod["reqMethod_count"])
+        .mul(100);
+
+      // availabilityCol.print();
+      finalTableMethod.addColumn({
+        column: "Availability (%)",
+        value: availabilityColMethod.col_data[0],
+      });
+
+      // finalTableMethod.print();
+
+      const outputTableMethod = finalTableMethod.loc({
+        columns: [
+          "reqMethod",
+          "reqMethod_count",
+          "cycleDuration_mean",
+          "Error (%)",
+          "Availability (%)",
+        ],
+      });
+
+      outputTableMethod["reqMethod_count"] = outputTableMethod[
+        "reqMethod_count"
+      ].div(3);
+
+      console.log("Final Table: ", outputTableMethod);
+      outputTableMethod.print();
+
+      outputTableMethod.data.forEach((row, i) => {
+        consolidatedObj.services[i].byMethod = {};
+        consolidatedObj.services[i].byMethod.method = row[0];
+        consolidatedObj.services[i].byMethod.status = "good";
+        consolidatedObj.services[i].byMethod.load = row[1].toFixed(2) + "hpm";
+        consolidatedObj.services[i].byMethod.response_time = row[2];
+        consolidatedObj.services[i].byMethod.error = row[3];
+        consolidatedObj.services[i].byMethod.availability = row[4];
+      });
+
+      console.log('FINAL OBJECT', consolidatedObj);
+    } catch (e) {
+      console.log("No query match in this row. Continuing...");
+    }
+  });
 
   // newErrorDFTotal.print();
 
   // const totalErrors = errorResRowsTotal.count().data;
   // totalErrors.print()
-  
+
   // dfNew.print();
 
   // let finalTable = dfd.merge({ left: dfNew, right: newErrorDF, on:['reqHost'], how: 'left' });
 
-  // const latencyDF = dfGroup.col(['cycleDuration']).mean(); 
-  
+  // const latencyDF = dfGroup.col(['cycleDuration']).mean();
 
   // finalTable = dfd.merge({ left: finalTable, right: latencyDF, on:['reqHost'], how: 'left' });
 
@@ -226,13 +357,13 @@ const rtData = (data) => {
   // // availabilityDF.print()
   // finalTable = dfd.merge({ left: finalTable, right: availabilityDF, on:['reqHost'], how: 'left' });
 
-  // finalTable.fillna({ values: [0], inplace: true }); 
+  // finalTable.fillna({ values: [0], inplace: true });
 
-  // //   const totalResponses = finalTable.loc({ columns: ['resStatusCode_1'] }); 
-  // //   const errorResponses = finalTable.loc({ columns: ['resStatusCode'] }); 
-  
+  // //   const totalResponses = finalTable.loc({ columns: ['resStatusCode_1'] });
+  // //   const errorResponses = finalTable.loc({ columns: ['resStatusCode'] });
+
   // const errorRateCol = finalTable[`resStatusCode_count`].div(finalTable[`resStatusCode_count_1`]).mul(100);
-  
+
   // //   const errorRate = errorRequests.div(totalRequests).mul(100);
   // //   // console.log(errorRate);
   // finalTable.addColumn({
@@ -240,11 +371,10 @@ const rtData = (data) => {
   //   value: errorRateCol.col_data[0],
   // });
 
-    
-  // // console.log(finalTable); 
-  
+  // // console.log(finalTable);
+
   // const availabilityCol = finalTable[`resStatusCode_count_1`].div(finalTable[`reqHost_count`]).mul(100);
-  
+
   // // availabilityCol.print();
   // finalTable.addColumn({
   //   column: 'Availability (%)',
@@ -255,59 +385,16 @@ const rtData = (data) => {
 
   // outputTable[`reqHost_count`] = outputTable[`reqHost_count`].div(3);
 
-  // console.log('Final Table: ', outputTable); 
-  // outputTable.print(); 
-  
+  // console.log('Final Table: ', outputTable);
+  // outputTable.print();
 
-
-
-
-  
-
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-  
   // console.log(dfNew);
-  
+
   // console.log(dfGroup);
-  // dfGroup.print() 
-  // dfGroup.col(['resStatusCode']).count().print(); 
-  
-  // dfGroup.print(); 
-  
+  // dfGroup.print()
+  // dfGroup.col(['resStatusCode']).count().print();
+
+  // dfGroup.print();
 
   // df.print();
   // group by host
@@ -316,39 +403,38 @@ const rtData = (data) => {
   // uniqueHosts.forEach((host) => {
   //   const dfHost = df.query({ column: "reqHost", is: "==", to: host }); //creates dataframe for each service
   //   const newObj = analyzer(dfHost);
-  //   dataframes.push(newObj); 
+  //   dataframes.push(newObj);
   // });
 
   // hostRows.print();
   // function analyzer(dfHost) {
-    
+
   //   const errorResRows = dfHost.query({ column: 'resStatusCode', is: ">=", to: 400 }); //creates dataframe for each service
   //   // errorTest.print()
-    
+
   //   // const groupErrorByHost = errorResRows.groupby(['reqHost', 'reqMethod']);
   //   // groupErrorByHost.col(['reqHost']).count().print();
   //   // console.log(groupErrorByHost);
   //   // const groupErrorTable = new dfd.DataFrame(groupErrorByHost.data);
   //   // groupErrorTable.print();
 
-  //   const groupErrorByMethod = errorResRows.groupby(['reqMethod']); 
+  //   const groupErrorByMethod = errorResRows.groupby(['reqMethod']);
   //   const rightTable = groupErrorByMethod.col(['reqMethod']).count();
 
-  //   const groupByMethod = dfHost.groupby(['reqMethod']); 
+  //   const groupByMethod = dfHost.groupby(['reqMethod']);
   //   const leftTable = groupByMethod.col(['reqMethod']).count();
 
-  //   // leftTable.print(); 
-    
-  //   const finalTable = dfd.merge({ left: leftTable, right: rightTable, on:['reqMethod'], how: 'left' }); 
+  //   // leftTable.print();
 
-  //   finalTable.fillna({ values: [0], inplace: true }); 
+  //   const finalTable = dfd.merge({ left: leftTable, right: rightTable, on:['reqMethod'], how: 'left' });
 
-    
+  //   finalTable.fillna({ values: [0], inplace: true });
+
   //   // finalTable.print();
 
-  //   const reqMethods = finalTable.loc({ columns: ['reqMethod'] }); 
-  //   const totalRequests = finalTable.loc({ columns: ['reqMethod_count'] }); 
-  //   const errorRequests = finalTable.loc({ columns: ['reqMethod_count_1'] }); 
+  //   const reqMethods = finalTable.loc({ columns: ['reqMethod'] });
+  //   const totalRequests = finalTable.loc({ columns: ['reqMethod_count'] });
+  //   const errorRequests = finalTable.loc({ columns: ['reqMethod_count_1'] });
 
   //   const errorRate = errorRequests.div(totalRequests).mul(100);
   //   // console.log(errorRate);
@@ -357,14 +443,14 @@ const rtData = (data) => {
   //     value: errorRate.col_data[0],
   //   });
 
-  //   // console.log(finalTableFilled); 
-  //   reqMethods.print(); 
+  //   // console.log(finalTableFilled);
+  //   reqMethods.print();
 
-  //   // const json = reqMethods.to_json({ orient: 'records' }); 
+  //   // const json = reqMethods.to_json({ orient: 'records' });
   //   reqMethods.to_json({ orient: 'records' })
   //     .then(json => json)
   //     .catch(err => console.log('Json error', err));
   // }
 };
 
-rtData(responseData); 
+rtData(responseData);
