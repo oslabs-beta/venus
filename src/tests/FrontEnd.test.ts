c14c4348e31d677a1b155d5e508126bb98cabe7b
Will Kencel <wkencel1@gmail.com>
2021-02-01 16:38:26 -0500

    testing commit

--- src/tests/FrontEnd.test.ts
+++ src/tests/FrontEnd.test.ts
@@ -124,7 +124,7 @@ import { render } from "@testing-library/react";
 import { SignIn } from "../containers/SignInContainer";
 
 it('expects button to be in SignIN component', () => {
-  const { queryByTestId } = render(<SignIn />;
+  const { queryByTestId } = render(SignIn());
   const addSignInButton = queryByTestId("signin-button");
   expect(addSignInButton).toBeInTheDocument();
 })
