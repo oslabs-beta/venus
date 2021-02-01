f309317f6cec9fcc264573006e06bfd622471a1d
Akshay Suggula <asuggula@umich.edu>
2021-02-01 00:10:43 -0800

    added write functionality for overall method level data

--- src/server/data_analysis/historical-data-analysis.js
+++ src/server/data_analysis/historical-data-analysis.js
@@ -185,7 +185,7 @@ const readAndWriteLastHour = () => {
   //Query for overall averages by method
   const selectOverallMethod = `SELECT MAX(timestamp) as timestamp, service, method, AVG(availability::int::float4) as availability, AVG(response_time::int::float4) as response_time, AVG(error_rate::int::float4) as error_rate, AVG(load::int::float4) as load FROM ${THREE_MIN_TABLE} WHERE timestamp >= ${Date.now() - HOUR}::BIGINT AND service = 'aggregate' AND method != 'aggregate' GROUP BY service, method;`;
 
-  client.query(selectOverallMethod, (err, result) => {
+  await client.query(selectOverallMethod, (err, result) => {
     if(err){
       console.log(err); 
     } else {
