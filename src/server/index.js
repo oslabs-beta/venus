0f880e747a4c2be85abd4c296d5e2adfd801053a
Akshay Suggula <asuggula@umich.edu>
2021-02-02 13:16:44 -0800

    hello world file

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,17 @@
 const express = require('express')
+const socket = require('socket.io'); 
+const bodyParser = require('body-parser'); 
+const cors = require('cors'); 
+
 const app = express()
 const port = 3000
+
+const data = require('./data_analysis/rt-data.js'); 
+const { constructHistorical, main, writeToDB } = require('./data_analysis/historical-data-analysis.js'); 
+const redis = require('./redis_handlers/real-time-read-handler.js'); 
+const authController = require('./controller.js')
+
+require('dotenv').config(); 
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
