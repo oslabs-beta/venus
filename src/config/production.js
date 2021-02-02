6f88b598da95c2efa4c763a170bc43287f8b85f6
Akshay Suggula <asuggula@umich.edu>
2021-02-01 23:37:46 -0800

    modified config for updated redis host

--- src/config/production.js
+++ src/config/production.js
@@ -12,8 +12,7 @@ module.exports = {
   },
   redisConnect: {
     port: 6379,
-    host: '127.0.0.1',
-    // host: 'venus-redis-micro.syohjt.ng.0001.use2.cache.amazonaws.com',
+    host: 'venus-redis-micro.syohjt.ng.0001.use2.cache.amazonaws.com',
     dnsLookup: (address, callback) => callback(null, address),
   },
   redisStream: {
