92a5900ca8310f8181916dc15db4ca48488a0f70
Akshay Suggula <asuggula@umich.edu>
2021-02-02 15:37:23 -0800

    converted string back into an object

--- src/server/index.js
+++ src/server/index.js
@@ -90,7 +90,7 @@ async function sendData(socket){
   if(streamData.length !== 0){
     //Analyze last three minutes of stream data and store output of analysis
     let output = data.rtData(streamData);
-    output = JSON.parse(output); 
+    output = JSON.parse(output[0]); 
     console.log('EMITTING VIA SOCKET'); 
     //Emit output to the front-end via websocket
     socket.emit('real-time-object', output); 
