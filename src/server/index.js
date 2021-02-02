26421031d3ebde99e8608aec77b0ef2b6975c81e
Akshay Suggula <asuggula@umich.edu>
2021-02-02 13:33:15 -0800

    hello world file

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const socket = require('socket.io'); 
 const bodyParser = require('body-parser'); 
-const cors = require('cors'); 
+// const cors = require('cors'); 
 
 const app = express()
 const port = 3000
@@ -13,7 +13,7 @@ const authController = require('./controller.js')
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(cors({origin: '*'})); 
+// app.use(cors({origin: '*'})); 
 app.use(bodyParser); 
 
 
@@ -23,6 +23,38 @@ require('dotenv').config();
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+// const io = socket(server); 
+
+/**
+ * socket.io "global handler" 
+ * At this point in the flow, a JWT access token has already been generated and stored in localStorage.
+ * This function verifies that the accessToken has been provided and subsequently verifies it.
+ */
+// io.use(function (socket, next) {
+//   if (socket.handshake.query && socket.handshake.query.accessToken) {
+//       console.log('SUCCESSFUL TOKEN HANDSHAKE')
+//       jwt.verify(socket.handshake.query.accessToken, ACCESS_SECRET, (err, decoded) => {
+//         if (err) return next(new Error('Token authentication error!'))
+//         socket.emit('')
+//         socket.decoded = decoded;
+//         return next();
+//       });
+//     } else {
+//       return next(new Error('Token authentication error!'))
+//     }
+// })
+
+// io.sockets.on('connection', (socket) => {
+//   console.log(`New connection: ${socket.id}`); 
+//   sendData(socket); 
+// })
+
+
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
